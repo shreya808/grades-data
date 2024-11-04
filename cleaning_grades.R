f60bd5eb1ea0a7156b563e57ce9463d202a2da03
shreya808 <shreyasinha@umail.ucsb.edu>
2024-11-03 17:55:16 -0800

    categorize departments by division

--- cleaning_grades.R
+++ cleaning_grades.R
@@ -1,10 +1,6 @@
 # Shreya Sinha
 # 11/01/2024
 
-
-# Make separate table with course code, division, department, and full course
-# name
-
 rm(list = ls())
 library(tidyverse)
 library(janitor)
@@ -92,6 +88,9 @@ cleaned_course_grades <- course_grades %>% mutate(
          total_Cs = C_plus + C_minus + C,
          total_Ds = D_plus + D_minus + D,
          total_Fs = F) %>% 
+  # column for quarter and year
+  mutate(quarter_year = paste(quarter, year, sep = " ")) %>% 
+# remove plus and minus grade columns and year column
   select(-A_plus, -A_minus, -A, -B_plus, -B_minus, -B, -C_plus, -C_minus, -C, -D_plus, -D_minus, -D, -F, -year) %>% 
   #create column for course number
   mutate(course_number = parse_number(course)) %>% 
@@ -99,17 +98,70 @@ cleaned_course_grades <- course_grades %>% mutate(
   # remove graduate classes (i.e. course number >= 200)
   filter(course_number < 200) %>% 
   # categorize division (i.e. if course is < 100 it is lower division, if course is >= 100 it is upper division)
-  mutate(division = case_when(
-    course_number < 100 ~ "lower",
-    course_number >= 100 ~ "upper"))
+  mutate(course_level = case_when(
+    course_number < 100 ~ "lower division",
+    course_number >= 100 ~ "upper division")) %>% 
+  # remove extra spaces in course code
+  mutate(course = str_squish(course)) %>% 
+  # rename department codes for two word codes
+  # regex finds matches from beginning of string in `course` column that
+  # have 2 words separated by spaces like "[word] [word]"
+  mutate(dept = if_else(str_detect(course, "^\\w+\\s+\\w+\\s"), 
+                        str_extract(course, "^\\w+\\s+\\w+\\s"),
+                        dept)) %>% 
+  # if dept has "W" at the end of the string, it is designated as an online course
+  # unique(course_grades$course[str_detect(course_grades$course, "\\s*\\w*W\\s*\\d+$")==TRUE])
+  mutate(online = if_else(str_detect(dept, "\\s*\\w*W\\s*$"), 1, 0)) %>%
+  # remove "W" from end of dept
+  mutate(dept = str_remove(dept, "W\\s*$")) %>% 
+  mutate(dept = str_squish(dept))
+           
+        
 
 # course grade percentage by instructor and quarter
 grade_percentage_instructor_quarter <- cleaned_course_grades %>% 
-  summarize(percentage_A = total_As / nLetterStudents,
-            .by = c("academic_year", "course", "quarter", "instructor", "dept", "avgGPA", "division"))
+  summarize(percentage_A = sum(total_As, na.rm = T) / sum(nLetterStudents, na.rm = T), 
+            .by = c("quarter_year", "academic_year", "course", "instructor", "dept", "avgGPA", "course_level", "quarter"))
 
 # course grade percentage by academic year
 grade_percentage_academic_year <- cleaned_course_grades %>% 
   summarize(percentage_A = sum(total_As, na.rm = T) / sum(nLetterStudents, na.rm = T),
             avg_GPA = mean(avgGPA, na.rm = T),
-            .by = c("academic_year", "course","dept", "division"))
+            .by = c("academic_year", "course","dept", "course_level"))
+
+# Make separate table with course code, division, department, and full course name
+
+# list of UCSB colleges
+college <- c("College of Letters & Science", "College of Engineering",
+             "College of Creative Studies",
+             "Gervitz Graduate School of Education",
+             "Bren School of Environmental Science & Management")
+
+course_info <- cleaned_course_grades %>%
+  select(course, dept, course_level) %>% 
+  distinct() %>% 
+  mutate(division = case_when(
+    # humanities and fine arts departments from https://www.hfa.ucsb.edu/academics/#majors-section
+    dept %in% c("ART", "ARTST", "CLASS", "C LIT", "EACS", "KOR", "JAPAN", "CHIN",
+                "ENGL", "FAMST","FLMST", "ITAL", "FR", "GER", "SLAV", "RUSS", "HIST",
+                "ARTHI", "LAIS", "LING", "LATIN","GREEK", "MUS", "MUS A", "PHIL", "RG ST", "HEB", "MES", "PORT", 
+                "SPAN", "DANCE", "THTR", "WRIT", "ME ST") ~ "Humanities and Fine Arts",
+    #social sciences departments from https://www.socialsciences.ucsb.edu/units
+    dept %in% c("ANTH", "AS AM", "BL ST", "CH ST", "COMM", "ECON", "ES", "ESS",
+                "FEMST", "GPS", "GLOBL", "MS", "POL S", "SOC", "LAWSO", "POL SH") ~ "Social Sciences",
+    # science departments from https://www.science.ucsb.edu/programs
+    dept %in% c("CHEM", "EARTH", "GEOL", "EEMB", "GEOG", "MATH", "MCDB", "ASTRO", "PHYS",
+                "PSY", "PSTAT", "ENV S", "SHS", "BMSE") ~ "Mathematical, Life, and Physical Sciences",
+    # engineering departments from https://engineering.ucsb.edu/departments
+    # there are no divisions in the college of engineering but for consistency
+    # the division for engineering courses will be "Engineering"
+    dept %in% c("CMPSC", "CH E", "BIOE", "ECE", "ENGR", "MATRL", "ME", "TMP") ~ "Engineering",
+    # division for "INT" classes will be "interdisciplinary"
+    dept %in% c("INT", "MAT") ~ "Interdisciplinary",
+    # division for "CS" classes will be "Creative Studies" 
+    # str_detect is finding dept where there is a space before "CS"; or dept is CMPSCCS or CMPTGCS or W&L (writing and literature)
+    str_detect(dept, "\\sCS") | dept == "CMPSCCS" | dept == "CMPTGCS" | dept == "W&L" ~ "Creative Studies",
+    # division for education or CNSCP will be "Graduate" because it is in the Gervitz Graduate School of Education
+    dept %in% c("ED", "CNCSP") ~ "Graduate"))
+
+course_info$dept[is.na(course_info$division)==TRUE]
