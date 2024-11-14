20d30d08c83f84032b130bcb28cddaad9bca31d8
shreya808 <shreyasinha@umail.ucsb.edu>
2024-11-14 02:03:08 -0800

    made plots for proportion of A's by college, division, and for specifically economics department

--- cleaning_grades.R
+++ cleaning_grades.R
@@ -1,9 +1,10 @@
 # Shreya Sinha
-# 11/01/2024
+# 11/13/2024
 
 rm(list = ls())
 library(tidyverse)
 library(janitor)
+library(lubridate)
 
 course_grades <- read_csv("courseGrades.csv")
 
@@ -114,20 +115,9 @@ cleaned_course_grades <- course_grades %>% mutate(
   mutate(online = if_else(str_detect(dept, "\\s*\\w*W\\s*$"), 1, 0)) %>%
   # remove "W" from end of dept
   mutate(dept = str_remove(dept, "W\\s*$")) %>% 
-  mutate(dept = str_squish(dept))
+  mutate(dept = str_squish(dept)) %>% 
+  mutate(total_students = nLetterStudents + nPNPStudents)
            
-        
-
-# course grade percentage by instructor and quarter
-grade_percentage_instructor_quarter <- cleaned_course_grades %>% 
-  summarize(percentage_A = sum(total_As, na.rm = T) / sum(nLetterStudents, na.rm = T), 
-            .by = c("quarter_year", "academic_year", "course", "instructor", "dept", "avgGPA", "course_level", "quarter"))
-
-# course grade percentage by academic year
-grade_percentage_academic_year <- cleaned_course_grades %>% 
-  summarize(percentage_A = sum(total_As, na.rm = T) / sum(nLetterStudents, na.rm = T),
-            avg_GPA = mean(avgGPA, na.rm = T),
-            .by = c("academic_year", "course","dept", "course_level"))
 
 # Make separate table with course code, division, department, and full course name
 
@@ -164,4 +154,167 @@ course_info <- cleaned_course_grades %>%
     division == "Engineering" ~ "College of Engineering",
     division == "Creative Studies" ~ "College of Creative Studies",
     division == "Graduate" ~ "Gervitz Graduate School of Education"))
+
+# join course_info with cleaned_course_grades
+course_division <- cleaned_course_grades %>%
+  left_join(course_info, by = c("course", "dept", "course_level")) 
+ 
+
+# course average grades weighted by number of students by academic year
+
+# average grade by college by year
+college_avg_grade <- course_division %>%
+  # find average GPA. percentage of A's, and total number of students enrolled for a course offered
+  # by multiple instructors/quarters in an academic year
+  summarize(course_avgGPA = weighted.mean(avgGPA, w = total_students, na.rm = T),
+            yearly_course_enrollment = sum(total_students, na.rm = T),
+            percentage_A = sum(total_As, na.rm = T) / sum(total_students, na.rm = T),
+            .by = c(academic_year, course, dept, division, college))  %>%
+  # find average GPA for each college per year
+  summarize(college_GPA = weighted.mean(course_avgGPA, w = yearly_course_enrollment, na.rm = T),
+            college_percentage_A = weighted.mean(percentage_A, w = yearly_course_enrollment, na.rm = T),
+            .by = c(academic_year, college))
+
+college_avg_plot <- college_avg_grade %>%
+  # filtering out interdisciplinary/interdepartmental courses
+  # filtering out college of creative studies because they do not have letter grades
+  filter(!is.na(college), college != "College of Creative Studies") %>%
+  ggplot(aes(x = academic_year, y = college_GPA, group = college)) +
+  geom_point(aes(colour = college))+
+  geom_line(aes(colour = college))+
+  labs(title = "Average GPA by Academic Year for UCSB Colleges",
+       x = "Academic Year",
+       y = "Grade Point Average",
+       colour = "College") +
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle = 45))
+
+print(college_avg_plot)
+
+ggsave('college_avg_plot.pdf')
+  
+            
+college_A_plot <- college_avg_grade %>%
+  # filtering out interdisciplinary/interdepartmental courses
+  # filtering out college of creative studies because they do not have letter grades
+  filter(!is.na(college), college != "College of Creative Studies") %>%
+  ggplot(aes(x = academic_year, y = college_percentage_A, group = college)) +
+  geom_point(aes(colour = college))+
+  geom_line(aes(colour = college))+
+  labs(title = "Proportion of A's by Academic Year for UCSB Colleges",
+       x = "Academic Year",
+       y = "Proportion of A's",
+       colour = "College") +
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle = 45))
+
+
+print(college_A_plot)
+ggsave('college_A_plot.pdf')
+
+
+# average grade by division by year 
+
+division_avg_grade <- course_division %>%
+  # find average GPA and total number of students enrolled for a course offered
+  # by multiple instructors/quarters in an academic year
+  summarize(course_avgGPA = weighted.mean(avgGPA, w = total_students, na.rm = T),
+            percentage_A = sum(total_As, na.rm = T) / sum(total_students, na.rm = T),
+            yearly_course_enrollment = sum(total_students, na.rm = T),
+            .by = c(academic_year, course, dept, division))  %>% 
+  # find average GPA for each division per year
+  summarize(division_GPA = weighted.mean(course_avgGPA, w = yearly_course_enrollment, na.rm = T),
+            division_percentage_A = weighted.mean(percentage_A, w = yearly_course_enrollment, na.rm = T),
+            .by = c(academic_year, division)) 
+  
+division_avg_plot <- division_avg_grade %>%
+  # filtering to not include Creative Studies and Engineering because they are not part of Letters & Science
+  filter(division != "Creative Studies", division != "Engineering") %>% 
+  ggplot(aes(x = academic_year, y = division_GPA, group = division)) +
+  geom_point(aes(colour = division))+
+  geom_line(aes(colour = division))+
+  labs(title = "Average GPA by Academic Year for Divisions of UCSB Letters & Science",
+       x = "Academic Year",
+       y = "Grade Point Average",
+       colour = "Letters & Science Division") +
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle = 45))
+  
+
+print(division_avg_plot)
+ggsave('division_avg_plot.pdf')
+
+division_A_plot <- division_avg_grade %>%
+  # filtering to not include Creative Studies and Engineering because they are not part of Letters & Science
+  filter(division != "Creative Studies" & division != "Engineering") %>%
+  ggplot(aes(x = academic_year, y = division_percentage_A, group = division)) +
+  geom_point(aes(colour = division))+
+  geom_line(aes(colour = division))+
+  labs(title = "Proportion of A's by Academic Year for Divisions of UCSB Letters & Science",
+       x = "Academic Year",
+       y = "Proportion of A's",
+       colour = "Letters & Science Division")+
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle = 45))
+
+  
+print(division_A_plot)
+ggsave('division_A_plot.pdf')
+  
+
+# average grade each year for econ department weighted by class size
+econ_avg_grade <- cleaned_course_grades %>%
+  filter(dept == "ECON") %>%
+  # find average GPA, proportion of As, and total number of students enrolled for a course offered
+  # by multiple instructors/quarters in an academic year
+  summarize(course_avgGPA = weighted.mean(avgGPA, w = total_students, na.rm = T),
+            percentage_A = sum(total_As, na.rm = T) / sum(total_students, na.rm = T),
+         yearly_course_enrollment = sum(total_students, na.rm = T),
+         .by = c(academic_year, course, course_level)) %>% 
+  # find average GPA and percentage As for Econ department per year
+  summarize(dept_avgGPA = weighted.mean(course_avgGPA, w = yearly_course_enrollment, na.rm = T),
+            dept_percentage_A = weighted.mean(percentage_A, w = yearly_course_enrollment, na.rm = T),
+        .by = c(academic_year, course_level))
+
+econ_avg_plot <- econ_avg_grade %>%
+  ggplot(aes(x = academic_year, y = dept_avgGPA, group = course_level)) +
+  geom_point(aes(colour = course_level))+
+  geom_line(aes(colour = course_level))+
+  labs(title = "Average Course GPA by Academic Year for UCSB Economics Department",
+       x = "Academic Year",
+       y = "Course GPA",
+       colour = "Course Level") +
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle = 45))
+
+print(econ_avg_plot)
+ggsave('econ_avg_plot.pdf')
+
+econ_A_plot <- econ_avg_grade %>%
+  ggplot(aes(x = academic_year, y = dept_percentage_A, group = course_level)) +
+  geom_point(aes(colour = course_level))+
+  geom_line(aes(colour = course_level))+
+  labs(title = "Proportion of A's by Academic Year for UCSB Economics Department",
+       x = "Academic Year",
+       y = "Proportion of A's",
+       colour = "Course Level") +
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle = 45))
+
+print(econ_A_plot)
+ggsave('econ_A_plot.pdf')
+
+
+
+# average grade by year for online vs in-person class
+
+
+# course grade percentage by academic year
+
+  # summarize(percentage_A = sum(total_As, na.rm = T) / sum(nLetterStudents, na.rm = T),
+  #           avg_GPA = mean(avgGPA, na.rm = T),
+  #           .by = c("academic_year", "course","dept", "course_level")) %>%
+  # summarize(percentage_A = mean(percentage_A, na.rm = T),
+  #           avg_GPA = mean(avg_GPA, na.rm = T),
+  #           .by = c("academic_year", "course_level"))
 
